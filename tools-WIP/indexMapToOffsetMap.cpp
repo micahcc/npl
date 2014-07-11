d6ff9df20419fd4a8c19bccda8b178c22b06b580
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-07-10 18:01:35 -0700

    Added tclap again, and template for nonlinear transform converter

--- tools-WIP/indexMapToOffsetMap.cpp
+++ tools-WIP/indexMapToOffsetMap.cpp
@@ -1,5 +1,34 @@
+#include <version.h>
+#include <tclap/CmdLine.h>
+#include <string>
+
+using std::string;
 
 int main()
 {
+	try {
+	/* 
+	 * Command Line 
+	 */
 
+	TCLAP::CmdLine cmd("Often nonlinear registration produces deformation fields"
+			" which store within each point a source point. Further these are"
+			" often in index coordinates rather than RAS coordinates. This tool "
+			"will convert a field of source indices into a field of offsets in "
+			"world coordinates. For example [x,y,z,] contains a vector with value "
+			"[x+a,y+b,z+c]. This is the sum of 3 vectors (x+a)i+(y+b)j+(z+c)k, we "
+			"change that to A[a,b,c]^T. Its also possible to invert (-I) .",
+			' ', __version__ );
+
+	TCLAP::ValueArg<string> a_fmri("i", "input", "Input image.", 
+			true, "", "*.nii.gz", cmd);
+	TCLAP::ValueArg<string> a_out("o", "out", "Output image.",
+			true, "", "*.nii.gz", cmd);
+	TCLAP::SwitchArg a_invert("I", "invert", "Whether to invert.", cmd);
+
+
+	
+
+	} catch (TCLAP::ArgException &e)  // catch any exceptions
+	{ std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; }
 }
