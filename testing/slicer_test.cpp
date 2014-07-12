1002d8f19907c47a5cba9e4da28ca557c5f3f762
Micah Chambers (troller) <micahcc@ucla.edu>
2014-07-11 23:37:04 -0700

    added license and preamble to code

--- testing/slicer_test.cpp
+++ testing/slicer_test.cpp
@@ -1,3 +1,22 @@
+/*******************************************************************************
+This file is part of Neural Program Library (NPL), 
+
+Written and Copyrighted by by Micah C. Chambers (micahc.vt@gmail.com)
+
+The Neural Program Library is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+The Neural Programs and Libraries are distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+the Neural Programs Library.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+
 
 #include "slicer.h"
 
