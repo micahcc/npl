d07c8526c4319d418be9cfc6b28f0281dc51e3e5
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-07-14 18:13:47 -0700

    added tool taht just reads a nifti and quits

---
+++ tools-WIP/niiHeader.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+This file is part of Neuro Programs and Libraries (NPL), 
+
+Written and Copyrighted by by Micah C. Chambers (micahc.vt@gmail.com)
+
+The Neuro Programs and Libraries is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+The Neural Programs and Libraries are distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+the Neural Programs Library.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+
+#include <version.h>
+#include <string>
+
+#include "mrimage.h"
+
+int main(int argc, char** argv)
+{
+	if(argc <= 1) {
+		return -1;
+	}
+
+	npl::readMRImage(argv[1], true);
+}
+
