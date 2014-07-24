e07937e90c1e9364d429ec4daab8f6928ae4cbd7
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-07-23 18:18:36 -0700

    added basic functions header with often-used functions

---
+++ lib/basic_functions.h
@@ -0,0 +1,56 @@
+/*******************************************************************************
+This file is part of Neuro Programs and Libraries (NPL), 
+
+Written and Copyrighted by by Micah C. Chambers (micahc.vt@gmail.com)
+
+The Neuro Programs and Libraries is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+The Neural Programs and Libraries are distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+the Neural Programs Library.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+
+/**
+ * @brief Clamps value to range of [inf, sup]. Values outside will be pulled 
+ * to either sup or inf, whichever is closer. 
+ *
+ * @tparam T Value of inf/sup/v/output
+ * @param inf Lower bound (infinum)
+ * @param sup Upper bound (supremum)
+ * @param v Value to clamp
+ *
+ * @return 
+ */
+template <typename T>
+inline T clamp(T inf, T sup, T v) 
+{
+	return std::min(sup, std::max(inf, v));
+}
+
+/**
+ * @brief Wraps and index based on the range [inf, sup] (when v is outside that)
+ * range. Thus inf = 1, sup = 5, v = 0 will wrap around to 4, and v = 6 would wrap 
+ * around to 1.
+ *
+ * @tparam T Value of inf/sup/v
+ * @param inf lower bound (inclusive)
+ * @param sup upper bound (inclusive)
+ * @param v valueto wrap. Output will be the position wrapped.
+ *
+ * @return Wrapped value
+ */
+template <typename T>
+T wrap(T inf, T sup, T v)
+{
+	T len = sup-inf+1;
+	T vtmp = v-inf;
+	T out = vtmp < 0 ? sup-((-vtmp-1)%len) : inf+(vtmp%len); 
+	return out;
+}
