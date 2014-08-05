14897460996e5e32f5cd68fda9502f6236855019
Micah Chambers (troller) <micahcc@ucla.edu>
2014-08-05 15:48:34 -0700

    working on a new slicer and updating old, and added a test for ROI

--- lib/slicer.cpp
+++ lib/slicer.cpp
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @file slicer.cpp
+ * @file slicer.cpp Contains definition for Slicer and Chunk slicer, two tools
+ * for sequentially walking through ND-spaces. 
  *
  *****************************************************************************/
 
@@ -100,7 +101,7 @@ Slicer& Slicer::operator--()
 			break;
 		} else if(ii != m_order.size()-1) {
 			// jump forward in dd, (will pull back in next)
-			m_linpos += m_roi[dd].second*m_strides[dd];
+			m_linpos += (m_roi[dd].second-m_pos[dd])*m_strides[dd];
 			m_pos[dd] = m_roi[dd].second;
 		}
 	}
@@ -372,5 +373,396 @@ void Slicer::goEnd()
 	m_linpos = m_linlast;
 	m_end = true;
 }
+
+/**
+ * @brief Default Constructor, max a length 1, dimension 1 slicer
+ */
+ChunkSlicer() : m_order(NULL), m_pos(NULL), m_roi(NULL), m_chunk(NULL),
+	m_dim(NULL), m_strides(NULL)
+{
+	size_t i=1;
+	setDim(1, &i);
+}
+
+/**
+ * @brief Constructor, takses the number of dimensions and the size of the
+ * image.
+ *
+ * @param ndim	size of ND array
+ * @param dim	array providing the size in each dimension
+ */
+ChunkSlicer(size_t ndim, const size_t* dim) : m_order(NULL), m_pos(NULL),
+	m_roi(NULL), m_chunk(NULL), m_dim(NULL), m_strides(NULL)
+
+{
+	setDim(ndim, dim);
+}
+
+
+/**
+ * @brief Sets the dimensionality of iteration, and the dimensions of the image
+ *
+ * Invalidates position and ROI.
+ *
+ * @param ndim Number of dimension
+ * @param dim Dimensions (size)
+ */
+ChunkSlicer::setDim(size_t ndim, const size_t* dim)
+{
+	m_ndim = ndim;
+	for(size_t ii=0; ii<ndim; ii++) {
+		if(ii < ndim) {
+			m_dim[ii] = dim[ii];
+		} else {
+			m_dim[ii] = 1;
+		}
+	}
+
+	// resetROI
+	setROI();
+	goBegin();
+}
+
+/*************************************
+ * Movement
+ ***********************************/
+
+/**
+ * @brief Prefix iterator. Iterates in the order dictatored by the dimension
+ * order passsed during construction or by setOrder
+ *
+ * @return 	new value of linear position
+ */
+ChunkSlicer& ChunkSlicer::operator++()
+{
+	if(isChunkEnd()) 
+		return *this;
+	
+	m_chunkend = true;
+	for(size_t ii=0; ii < m_dim; ii++){
+		size_t dd = m_order[ii];
+		if(m_pos[dd] < m_chunk[dd].second) {
+			m_pos[dd]++;
+			m_linpos += m_strides[dd];
+			m_chunkend = false;
+			break;
+		} else {
+			// rool over 
+			m_linpos -= (m_pos[dd]-m_chunk[dd].first)*m_strides[dd];
+			m_pos[dd] = m_chunk[dd].first;
+		}
+	}
+};
+
+/**
+ * @brief Prefix negative  iterator. Iterates in the order dictatored by
+ * the dimension order passsed during construction or by setOrder
+ *
+ * @return 	new value of linear position
+ */
+ChunkSlicer& ChunkSlicer::operator--()
+{
+	if(isChunkBegin()) 
+		return *this;
+
+	m_chunkend = false;
+	for(size_t ii=0; ii < m_dim; ii++){
+		size_t dd = m_order[ii];
+		if(m_pos[dd] > m_chunk[dd].first) {
+			m_pos[dd]--;
+			m_linpos -= m_strides[dd];
+			break;
+		} else {
+			// rool over 
+			m_linpos += (m_chunk[dd].second-m_pos[dd])*m_strides[dd];
+			m_pos[dd] = m_chunk[dd].second;
+		}
+	}
+};
+
+/**
+ * @brief Proceed to the next chunk (if there is one).
+ *
+ * @return 	
+ */
+ChunkSlicer& ChunkSlicer::nextChunk()
+{
+	if(isEnd()) 
+		return *this;
+	
+	// try to move in whatever dimension can be stepped
+	m_end = true;
+	for(size_t ii=0; ii < m_dim; ii++){
+		size_t dd = m_order[ii];
+		if(m_chunk[dd].second < m_roi[dd].second) {
+			// increment beginning chunk 
+			m_chunk[dd].first = m_chunk[dd].second+1;
+
+
+			// update second
+			if(m_chunksizes[dd] + m_chunk[dd].first - 1 > m_roi[dd].second || 
+						m_chunksizes[dd] == 0) 
+				m_chunk[dd].second = m_roi[dd].second;
+			else 
+				m_chunk[dd].second = m_chunksizes[dd] + m_chunk[dd].first - 1;
+				
+			m_end = false;
+			break;
+		} else {
+			// rool over beginning of chunk
+			m_chunk[dd].first = m_roi[dd].first;
+			
+			// update second
+			if(m_chunksizes[dd] + m_chunk[dd].first - 1 > m_roi[dd].second || 
+						m_chunksizes[dd] == 0) 
+				m_chunk[dd].second = m_roi[dd].second;
+			else 
+				m_chunk[dd].second = m_chunksizes[dd] + m_chunk[dd].first - 1;
+		}
+	}
+
+	// reset position
+	m_linpos = 0;
+	m_chunkfirst = 0;
+	for(size_t ii=0; ii<m_dim; ii++) {
+		m_pos[ii] = m_chunk[ii].first;
+		m_chunkfirst += m_pos[ii]*m_strides[ii];
+		m_linpos += m_pos[ii]*m_strides[ii];
+	}
+}
+
+/**
+ * @brief Return to the previous chunk (if there is one).
+ *
+ * @return 	new value of linear position
+ */
+ChunkSlicer& ChunkSlicer::prevChunk()
+{
+	m_chunkend = false;
+	m_end = false;
+};
+
+/**
+ * @brief Go to the beginning for the current chunk
+ *
+ */
+void ChunkSlicer::goChunkBegin()
+{
+	m_chunkend = false;
+	m_end = false;
+}
+
+///**
+// * @brief Jump to the end of current chunk.
+// *
+// */
+//void ChunkSlicer::goChunkEnd()
+//{
+//	if
+//	m_end = false;
+//	m_chunkend = false;
+//};
+//
+/**
+ * @brief Go to the very beginning for the first chunk.
+ *
+ * Depends on m_roi
+ *
+ * Set m_end, m_chunkend, m_linpos, m_linfirst, m_chunkfirst, m_pos, m_chunk
+ *
+ */
+void ChunkSlicer::goBegin()
+{
+	m_end = false;
+	m_chunkend = false;
+	
+	m_pos.resize(m_dim);
+	m_chunk.resize(m_dim);
+
+	m_linpos = 0;
+	m_linfirst = 0;
+	m_chunkfirst = 0;
+	for(size_t ii=0; ii<m_dim; ii++) {
+		m_pos[ii] = m_roi[ii].first;
+		m_linpos += m_pos[ii]*m_strides[ii];
+		m_linfirst += m_pos[ii]*m_strides[ii];
+		m_chunkfirst += m_pos[ii]*m_strides[ii];
+		m_chunk[ii].first = m_roi[ii].first;
+		if(m_chunksizes[ii] == 0) 
+			m_chunk[ii].second = m_roi[ii].second; 
+		else 
+			m_chunk[ii].second = m_chunk[ii].first + m_chunksizes[ii] - 1;
+	}
+}
+//
+///**
+// * @brief Jump to the end of the last chunk.
+// *
+// */
+//void ChunkSlicer::goEnd()
+//{
+//	// end is a special state, we go to end and leave end specially since its
+//	// actual location would depened on ROI, order etc.
+//	m_end = true;
+//	m_chunkend = true;
+//};
+//
+/**
+ * @brief Jump to the given position, additional values in newpos beyond dim
+ * will be ignored. Any values missing due to ndim > len will be treated as
+ * zeros.
+ *
+ * @param newpos	location to move to
+ */
+void ChunkSlicer::goIndex(size_t len, int64_t* newpos)
+{
+	m_end = false;
+	m_chunkend = false;
+};
+
+/**
+ * @brief Jump to the given position
+ *
+ * @param newpos	location to move to
+ */
+void ChunkSlicer::goIndex(std::vector<int64_t> newpos);
+
+/****************************************
+ *
+ * Actually get the linear location
+ *
+ ***************************************/
+
+/**
+ * @brief Places the first len dimension in the given array. If the number
+ * of dimensions exceed the len then the additional dimensions will be
+ * ignored, if len exceeds the dimensionality then index[dim...len-1] = 0.
+ * In other words index will be completely overwritten in the most sane way
+ * possible if the internal dimensions and size index differ.
+ *
+ * @param ndim size of index
+ * @param index output index variable
+ */
+void ChunkSlicer::index(size_t len, int64_t* index) const;
+
+/***********************************************
+ *
+ * Modification
+ *
+ **********************************************/
+
+/**
+ * @brief Sets the region of interest. During iteration or any motion the
+ * position will not move outside the specified range. Extra elements in
+ * roi beyond the number of dimensions, are ignored.
+ *
+ * Invalidates position
+ *
+ * @param roi	pair of [min,max] values in the desired hypercube
+ */
+void ChunkSlicer::setROI(const std::vector<std::pair<int64_t, int64_t>>& roi)
+{
+	m_roi.resize(m_dim);
+	for(size_t ii=0; ii<m_dim; ii++) {
+		if(ii < roi.size()) {
+			m_roi[ii].first = roi[ii].first;
+			m_roi[ii].second = roi[ii].second;
+		} else {
+			m_roi[ii].first = 0;
+			m_roi[ii].second = m_dim[ii]-1;
+		}
+	}
+}
+
+/**
+ * @brief Sets the region of interest. During iteration or motion the
+ * position will not move outside the specified range
+ *
+ * Invalidates position
+ *
+ * @param len	Length of both lower and upper arrays.
+ * @param lower	Coordinate at lower bound of bounding box.
+ * @param upper	Coordinate at upper bound of bounding box.
+ * @param roi	pair of [min,max] values in the desired hypercube
+ */
+void ChunkSlicer::setROI(size_t len, const int64_t* lower, const int64_t* upper)
+{
+	if(lower == NULL || upper == NULL)
+		len = 0;
+
+	m_roi.resize(m_dim);
+	for(size_t ii=0; ii<m_dim; ii++) {
+		if(ii < len) {
+			m_roi[ii].first = lower[ii]);
+			m_roi[ii].second = upper[ii]);
+		} else {
+			m_roi[ii].first = 0;
+			m_roi[ii].second = m_dim[ii]-1;
+		}
+	}
+}
+
+/**
+ * @brief Set the sizes of chunks for each dimension. Chunks will end every
+ * N steps in each of the provided dimension, with the caveout that 0
+ * indicates no breaks in the given dimension. So size = {0, 2, 2} will
+ * cause chunks to after {XLEN-1, 1, 1}. {0,0,0} (the default) indicate
+ * that the entire image will be iterated and only one chunk will be used. 
+ *
+ * Invalidates position
+ *
+ * @param len Length of sizes array
+ * @param sizes Size of chunk in each dimension. If you multiply together
+ * the elements of sizes that is the MAXIMUM number of iterations between 
+ * chunks. Note however that there could be less if we are at the edge.
+ */
+void ChunkSlicer::setChunkSize(size_t len, const int64_t* sizes)
+{
+	assert(m_roi.size() == m_ndim);
+	m_chunk.resize(m_ndim);
+	m_chunksizes.resize(m_ndim);
+
+	// missing values are assumed to be full (0)
+	for(size_t ii=0; ii<m_ndim; ii++) {
+		if(ii<len && sizes[ii] > 0) {
+			m_chunksizes[ii] = sizes[ii];
+		} else { 
+			m_chunksizes[ii] = 0;
+		}
+	}
+}
+
+/**
+ * @brief Set the sizes of chunks for each dimension. Chunks will end every
+ * N steps in each of the provided dimension, with the caveout that 0
+ * indicates no breaks in the given dimension. So size = {0, 2, 2} will
+ * cause chunks to after {XLEN-1, 1, 1}. {0,0,0} (the default) indicate
+ * that the entire image will be iterated and only one chunk will be used. 
+ *
+ * Invalidates position
+ *
+ * @param len Length of sizes array
+ * @param sizes Size of chunk in each dimension. If you multiply together
+ * the elements of sizes that is the MAXIMUM number of iterations between 
+ * chunks. Note however that there could be less if we are at the edge.
+ */
+void ChunkSlicer::setBreaks(size_t len, const int64_t* sizes);
+
+/**
+ * @brief Sets the order of iteration from ++/-- operators
+ *
+ * Invalidates position
+ *
+ * @param order	vector of priorities, with first element being the fastest
+ * iteration and last the slowest. All other dimensions not used will be
+ * slower than the last
+ * @param revorder	Reverse order, in which case the first element of order
+ * 					will have the slowest iteration, and dimensions not
+ * 					specified in order will be faster than those included.
+ */
+void ChunkSlicer::setOrder(const std::vector<size_t>& order, bool revorder);
+
+
+};
 	
 } //npl
--- lib/slicer.h
+++ lib/slicer.h
@@ -29,11 +29,11 @@ using namespace std;
 namespace npl {
 
 /**
- * @brief This class is used to slice an image in along a dimension, and to
- * step an arbitrary direction in an image. Order may be any size from 0 to the
- * number of dimensions. The first member of order will be the fastest moving,
- * and the last will be the slowest. Any not dimensions not included in the order
- * vector will be slower than the last member of order
+ * @brief This class is used to step through an ND array in order of dimensions.
+ * Order may be any size from 0 to the number of dimensions. The first member
+ * of order will be the fastest moving, and the last will be the slowest. Any
+ * not dimensions not included in the order vector will be slower than the last
+ * member of order
  */
 class Slicer
 {
@@ -240,6 +240,309 @@ protected:
 	 * @param dim	size of nd array, number of dimesions given by dim.size()
 	 */
 	void updateDim(size_t ndim, const size_t* dim);
+};
+
+/**
+ * @brief This class is used to step through an ND array in order of
+ * dimensions, but unlike Slicer it breaks the NDArray into chunks. Iteration
+ * stops at the end of each chunk until nextChunk is called. By default only
+ * one chunk is used, equal to the entire image. Use setBreaks() to set the
+ * frequency of chunks. The input to setBreaks() is an array of integers, where
+ * the next chunk occurs when dist%break == 0, with the special property that
+ * break=0 indicates no breaks for the dimension. So for a 3D image,
+ * setBreaks({1,0,0}) will stop every time a new x-values is reached. Note that
+ * the affects the order of iteration, so x cannot be the fastest iterator.
+ * setChunkSize() is an alias for setBreaks. Note that in cases where
+ * break%size != 0, for example image size = 5 and break = 3, chunk sizes will
+ * differ during the course of iteration!
+ *
+ * Order may still be set to decide the order of iteration. The first member
+ * of setOrder will be the fastest moving, and the last will be the slowest.
+ * Any dimensions not included in the order vector will be slower than the last
+ * member of order. Note that order will not necessarily be strictly obeyed
+ * when more than one chunk size is > 0. For isntance if setBreaks({1,1,0}), 
+ * setOrder({0,1,2}) are used, then all of 2 will visited before we iterate in
+ * 0 or 1, because otherwise we would be stepping across chunks.
+ */
+class ChunkSlicer 
+{
+public:
+	
+	/****************************************
+	 *
+	 * Constructors
+	 *
+	 ****************************************/
+
+	/**
+	 * @brief Default Constructor, max a length 1, dimension 1 slicer
+	 */
+	ChunkSlicer();
+
+	/**
+	 * @brief Constructor, takses the number of dimensions and the size of the
+	 * image.
+	 *
+	 * @param ndim	size of ND array
+	 * @param dim	array providing the size in each dimension
+	 */
+	ChunkSlicer(size_t ndim, const size_t* dim);
+	
+	/****************************************
+	 *
+	 * Query Location
+	 *
+	 ****************************************/
+	
+	/**
+	 * @brief Are we at the begining of iteration?
+	 *
+	 * @return true if we are at the begining
+	 */
+	bool isBegin() const { return m_linpos==m_linfirst; };
+
+	/**
+	 * @brief Are we at the end of iteration? Note that this will be 1 past the
+	 * end, as typically is done in c++
+	 *
+	 * @return true if we are at the end
+	 */
+	bool isEnd() const { return m_end; };
+	
+	/**
+	 * @brief Are we at the end of iteration? Note that this will be 1 past the
+	 * end, as typically is done in c++
+	 *
+	 * @return true if we are at the end
+	 */
+	bool eof() const { return m_end; };
+
+	/**
+	 * @brief Returns true when we have reached the end of the chunk.
+	 *
+	 * @return Are we ready for the next chunk?
+	 */
+	bool isChunkBegin() const { return m_linpos==m_chunkfirst; };
+
+	/**
+	 * @brief Returns true when we have reached the end of the chunk.
+	 *
+	 * @return Are we ready for the next chunk?
+	 */
+	bool isChunkEnd() const { return m_chunkend; };
+
+	/**
+	 * @brief Returns true when we have reached the end of the chunk.
+	 *
+	 * @return Are we ready for the next chunk?
+	 */
+	bool eoc() const { return m_chunkend; };
+
+	/*************************************
+	 * Movement
+	 ***********************************/
+
+	/**
+	 * @brief Prefix iterator. Iterates in the order dictatored by the dimension
+	 * order passsed during construction or by setOrder
+	 *
+	 * @return 	new value of linear position
+	 */
+	ChunkSlicer& operator++();
+	
+	/**
+	 * @brief Prefix negative  iterator. Iterates in the order dictatored by
+	 * the dimension order passsed during construction or by setOrder
+	 *
+	 * @return 	new value of linear position
+	 */
+	ChunkSlicer& operator--();
+	
+	/**
+	 * @brief Proceed to the next chunk (if there is one).
+	 *
+	 * @return 	
+	 */
+	ChunkSlicer& nextChunk();
+	
+	/**
+	 * @brief Return to the previous chunk (if there is one).
+	 *
+	 * @return 	new value of linear position
+	 */
+	ChunkSlicer& prevChunk();
+
+	/**
+	 * @brief Go to the beginning for the current chunk
+	 *
+	 */
+	void goChunkBegin();
+
+	/**
+	 * @brief Jump to the end of current chunk.
+	 *
+	 */
+	void goChunkEnd();
+
+	/**
+	 * @brief Go to the very beginning for the first chunk
+	 *
+	 */
+	void goBegin();
+
+	/**
+	 * @brief Jump to the end of the last chunk.
+	 *
+	 */
+	void goEnd();
+
+	/**
+	 * @brief Jump to the given position, additional values in newpos beyond dim
+	 * will be ignored. Any values missing due to ndim > len will be treated as
+	 * zeros.
+	 *
+	 * @param newpos	location to move to
+	 */
+	void goIndex(size_t len, int64_t* newpos);
+	
+	/**
+	 * @brief Jump to the given position
+	 *
+	 * @param newpos	location to move to
+	 */
+	void goIndex(std::vector<int64_t> newpos);
+
+	/****************************************
+	 *
+	 * Actually get the linear location
+	 *
+	 ***************************************/
+
+	/**
+	 * @brief dereference operator. Returns the linear position in the array
+	 * given the n-dimensional position.
+	 *
+	 * @return
+	 */
+	inline
+	int64_t operator*() const { return m_linpos; };
+	
+	/**
+	 * @brief Places the first len dimension in the given array. If the number
+	 * of dimensions exceed the len then the additional dimensions will be
+	 * ignored, if len exceeds the dimensionality then index[dim...len-1] = 0.
+	 * In other words index will be completely overwritten in the most sane way
+	 * possible if the internal dimensions and size index differ.
+	 *
+	 * @param ndim size of index
+	 * @param index output index variable
+	 */
+	void index(size_t len, int64_t* index) const;
+
+	/***********************************************
+	 *
+	 * Modification
+	 *
+	 **********************************************/
+
+	/**
+	 * @brief Sets the region of interest. During iteration or any motion the
+	 * position will not move outside the specified range. Extra elements in
+	 * roi beyond the number of dimensions, are ignored
+	 *
+	 * @param roi	pair of [min,max] values in the desired hypercube
+	 */
+	void setROI(const std::vector<std::pair<int64_t, int64_t>>& roi);
+
+	/**
+	 * @brief Sets the region of interest. During iteration or motion the
+	 * position will not move outside the specified range
+	 *
+	 * @param len	Length of both lower and upper arrays.
+	 * @param lower	Coordinate at lower bound of bounding box.
+	 * @param upper	Coordinate at upper bound of bounding box.
+	 * @param roi	pair of [min,max] values in the desired hypercube
+	 */
+	void setROI(size_t len = 0, const int64_t* lower = NULL, 
+			const int64_t* upper = NULL);
+
+	/**
+	 * @brief Set the sizes of chunks for each dimension. Chunks will end every
+	 * N steps in each of the provided dimension, with the caveout that 0
+	 * indicates no breaks in the given dimension. So size = {0, 2, 2} will
+	 * cause chunks to after {XLEN-1, 1, 1}. {0,0,0} (the default) indicate
+	 * that the entire image will be iterated and only one chunk will be used. 
+	 *
+	 * @param len Length of sizes array
+	 * @param sizes Size of chunk in each dimension. If you multiply together
+	 * the elements of sizes that is the MAXIMUM number of iterations between 
+	 * chunks. Note however that there could be less if we are at the edge.
+	 */
+	void setChunkSize(size_t len, const int64_t* sizes);
+
+	/**
+	 * @brief Set the sizes of chunks for each dimension. Chunks will end every
+	 * N steps in each of the provided dimension, with the caveout that 0
+	 * indicates no breaks in the given dimension. So size = {0, 2, 2} will
+	 * cause chunks to after {XLEN-1, 1, 1}. {0,0,0} (the default) indicate
+	 * that the entire image will be iterated and only one chunk will be used. 
+	 *
+	 * @param len Length of sizes array
+	 * @param sizes Size of chunk in each dimension. If you multiply together
+	 * the elements of sizes that is the MAXIMUM number of iterations between 
+	 * chunks. Note however that there could be less if we are at the edge.
+	 */
+	void setBreaks(size_t len, const int64_t* sizes);
+
+	/**
+	 * @brief Sets the order of iteration from ++/-- operators
+	 *
+	 * @param order	vector of priorities, with first element being the fastest
+	 * iteration and last the slowest. All other dimensions not used will be
+	 * slower than the last
+	 * @param revorder	Reverse order, in which case the first element of order
+	 * 					will have the slowest iteration, and dimensions not
+	 * 					specified in order will be faster than those included.
+	 */
+	void setOrder(const std::vector<size_t>& order, bool revorder = false);
+
+	/**
+	 * @brief Returns the array giving the order of dimension being traversed.
+	 * So 3,2,1,0 would mean that the next point in dimension 3 will be next,
+	 * when wrapping the next point in 2 is visited, when that wraps the next
+	 * in one and so on.
+	 *
+	 * @return Order of dimensions
+	 */
+	const std::vector<size_t>& getOrder() const { return m_order; } ;
+
+protected:
+	
+	/******************************************
+	 *
+	 * Offset, useful to kernel processing
+	 *
+	 ******************************************/
+	
+	// used to determine begin/end status
+	size_t m_linfirst;
+	size_t m_chunkfirst;
+	bool m_end;
+	bool m_chunkend;
+	
+	// position
+	size_t m_linpos;
+	std::vector<size_t> m_pos;
+	std::vector<std::pair<int64_t,int64_t>> m_chunk;
+	
+	// relatively static
+	std::vector<size_t>  m_order;
+	std::vector<std::pair<int64_t,int64_t>> m_roi;
+	std::vector<int64_t> m_chunksizes;
+
+	size_t m_ndim;
+	std::vector<size_t> m_dim;
+	std::vector<size_t> m_strides;
 };
 
 } // npl
