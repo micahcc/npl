e3bac7e561b40c43372b10af618d1e17f2994686
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-08-07 14:44:07 -0700

    added fft_shear_test which is not finished yet

---
+++ testing/fft_shear_test.cpp
@@ -0,0 +1,138 @@
+/******************************************************************************
+ * Copyright 2014 Micah C Chambers (micahc.vt@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file shear_test.cpp Test to show that shearing using the fourier transform 
+ * on each line works as well as manual shearing.
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ * @file fft_test.cpp
+ * @brief This file is specifically to test forward, reverse of fft image
+ * procesing functions.
+ ******************************************************************************/
+
+#include <version.h>
+#include <string>
+#include <stdexcept>
+
+#include "mrimage.h"
+#include "mrimage_utils.h"
+#include "ndarray_utils.h"
+#include "iterators.h"
+#include "accessors.h"
+
+using namespace npl;
+using namespace std;
+
+int closeCompare(shared_ptr<const MRImage> a, shared_ptr<const MRImage> b)
+{
+	if(a->ndim() != b->ndim()) {
+		cerr << "Error image dimensionality differs" << endl;
+		return -1;
+	}
+	
+	for(size_t dd=0; dd<a->ndim(); dd++) {
+		if(a->dim(dd) != b->dim(dd)) {
+			cerr << "Image size in the " << dd << " direction differs" << endl;
+			return -1;
+		}
+	}
+
+	OrderConstIter<double> ita(a);
+	OrderConstIter<double> itb(b);
+	itb.setOrder(ita.getOrder());
+	for(ita.goBegin(), itb.goBegin(); !ita.eof() && !itb.eof(); ++ita, ++itb) {
+		double diff = fabs(*ita - *itb);
+		if(diff > 1E-10) {
+			cerr << "Images differ!" << endl;
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+shared_ptr<MRImage> manualShearImage(shared_ptr<MRImage> in, size_t dim,
+		size_t len, double* dist)
+{
+	int64_t index[3];
+	double cindex[3];
+	std::vector<double> inv(in->ndim(), 1);
+	for(size_t ii=0; ii<inv.size() && ii<len; ii++) 
+		inv[ii] = 1./dist[ii];
+
+	auto mshear = dynamic_pointer_cast<MRImage>(in->copy());
+	LinInterp3DView<double> lininterp(in);
+
+	// stop at the end of x lines
+	ChunkIter<double> it(mshear);
+	std::vector<int64_t> csize(in->ndim(), 1);
+	csize[dim] = 0;
+	it.setChunkSize(dim, csize.data()); 
+	for(it.goBegin(); !it.eof(); it.nextChunk()) {
+		for(; !it.isChunkEnd(); ++it) {
+			it.index(3, index);
+
+			for(size_t ii=0; ii<3; ii++) {
+				cindex[ii] = inv[ii]*index[ii];
+			}
+
+			// grab value
+			it.set(lininterp(cindex[0], cindex[1], cindex[2]));
+		}
+	}
+	return mshear;
+}
+
+int main()
+{
+	int64_t index[3];
+	// create an image
+	size_t sz[] = {128, 128, 128};
+	cerr << sizeof(sz) << endl;
+	auto in = createMRImage(sizeof(sz)/sizeof(size_t), sz, FLOAT64);
+
+	// fill with sphere
+	OrderIter<double> sit(in);
+	while(!sit.eof()) {
+		sit.index(3, index);
+		double dist = 0;
+		for(size_t ii=0; ii<3 ; ii++) {
+			dist += (index[ii]-sz[ii]/2.)*(index[ii]-sz[ii]/2.);
+		}
+		if(sqrt(dist) < 5)
+			sit.set(1);
+		else
+			sit.set(0);
+
+		++sit;
+	}
+	
+	double shear[3] = {1, 5, 10};
+
+	// manual shear
+	auto mshear = manualShearImage(in, 0, 3, shear);
+	mshear->write("manual_shear.nii.gz");
+	
+//	shearImage(in, 0, shear);
+//	in->write("fourier_shear.nii.gz");
+//
+//	if(closeCompare(in, mshift) != 0)
+//		return -1;
+	
+
+	return 0;
+}
