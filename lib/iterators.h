25cfe0725b864f0e38e1d12b478b6009154e4470
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-10-11 13:27:39 -0700

    small changes to biascorrect

--- lib/iterators.h
+++ lib/iterators.h
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @file iterators.h
+ * @file iterators.h Iterators for images. Each is templated by the type that 
+ * the pixels are viewed as (rather than the actual stored pixel type).
  *
  *****************************************************************************/
 
