64639bc6964e27a5e44dc436fbdaefb1f8ae324a
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-10-27 09:50:01 -0700

    Switched back to old downsample Smooth function

--- lib/basic_functions.h
+++ lib/basic_functions.h
@@ -187,7 +187,7 @@ double wingaussWindow(double x, double a)
 {
 	const double sd = 5;
 	if(fabs(x) < a)
-		return exp(-x*x2*M_PI*M_PI*sd*sd/(a*a));
+		return exp(-x*x*2*M_PI*M_PI*sd*sd/(a*a));
 	else
 		return 0;
 }
