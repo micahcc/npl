6453e0a07efdb39edd84d5116783cdbf31773047
Micah Chambers (scrapper) <micahcc@ucla.edu>
2014-10-28 21:17:07 -0700

    work on new ICA tool

--- tools/fmri_ica.cpp
+++ tools/fmri_ica.cpp
@@ -41,36 +41,7 @@ MatrixXd reduce(shared_ptr<const MRImage> in)
 {
     if(in->ndim() != 4)
         throw INVALID_ARGUMENT(": Input mmust be 4D!");
-    
-    // fill Matrix with values from input
-    size_t T = in->tlen();
-    size_t N = in->elements()/T;
-
-    // fill, zero mean the timeseries
-    MatrixXd data(T, N);
-    ChunkConstIter<double> it(in); 
-    it.setLineChunk(3);
-    for(size_t xx=0; !it.eof(); it.nextChunk(), ++xx) {
-        double tmp = 0;
-        for(size_t tt=0; !it.eoc(); ++it) {
-            data(tt,xx) = *it;
-            tmp += *it;
-        }
-        tmp = 1./tmp;
-        for(size_t tt=0; !it.eoc(); ++it) 
-            data(tt,xx) = data(tt,xx)*tmp;
-    }
-
-    // perform PCA
-	std::cerr << "PCA...";
-	MatrixXd X_pc = pca(data, 0.01);
-	std::cerr << "Done " << endl;
 	
-    // perform ICA
-	std::cerr << "ICA...";
-	MatrixXd X_ic = ica(X_pc, 0.01);
-	std::cerr << "Done" << endl;
-
     return X_ic;
 }
 
@@ -81,11 +52,28 @@ int main(int argc, char** argv)
 	 * Command Line
 	 */
 
-	TCLAP::CmdLine cmd("Perform ICA analysis on an image. ",
-            ' ', __version__ );
+	TCLAP::CmdLine cmd("Perform ICA analysis on an image, or group of images. "
+			"By default this will be a temporal ICA. You can perform "
+			"group-analysis by concatinating in space or time. ORDER OF INPUTS "
+			"MATTERS IF YOU CONCATINATE IN BOTH. Concatination in time occurs "
+			"for adjacent arguments. So for -t 2 -s 2 -i a -i b -i c -i d"
+			"a and b will be concatinated in time and c and d will be "
+			"concatinated in time then ab and cd will be concatined in space.",
+			' ', __version__ );
 
-	TCLAP::ValueArg<string> a_in("i", "input", "Input fMRI image.",
-			true, "", "*.nii.gz", cmd);
+	TCLAP::SwitchArg a_spatial_ica("S", "spatial-ica", "Perform a spatial ICA"
+			", reducing unmixing timepoints to produce spatially independent "
+			"maps.", cmd);
+
+	TCLAP::MultiArg<string> a_in("i", "input", "Input fMRI image.",
+			true, "*.nii.gz", cmd);
+	TCLAP::ValueArg<int> a_time_append("t", "time-appends", "Number of images "
+			"to append in the matrix of images, in the time direction.", false,
+			0, "int", cmd);
+	TCLAP::ValueArg<int> a_space_append("s", "space-appends", "Number of images "
+			"to append in the matrix of images, in the space direction.", false,
+			0, "int", cmd);
+
     TCLAP::ValueArg<string> a_components("o", "out-components", "Output "
             "Independent Components as a 1x1xCxT image.",
 			true, "", "*.nii.gz", cmd);
@@ -102,24 +90,122 @@ int main(int argc, char** argv)
 	/**********
 	 * Input
 	 *********/
-	auto inimg = readMRImage(a_in.getValue());
-	if(inimg->ndim() != 4) {
-		cerr << "Expected input to be 4D Image!" << endl;
+	if(!a_in.isSet()) {
+		cerr << "Need to provide at least 1 input image!" << endl;
 		return -1;
 	}
 
-    // 
-	MatrixXd regressors = reduce(inimg);
-    for(size_t cc = 0; cc < regressors.cols(); cc++) {
-        // perform regression
-        //RegrResult tmp = regress(inimg, regressors.row(cc));
+	if(a_spatial_ica.isSet()) {
+		cout << "Performing Spatial ICA" << endl;
+		// Covariance Method (since presumably there will be more voxels
+		// than timepoints
+		
+		// fill Matrix with values from inputs, skip first (just use already-read)
+		size_t T = ref->tlen();
+		size_t N = ref->elements()/T;
+		for(
 
-        // write out each of the images
-        //tmp.rsqr->write("rsqr_"+to_string(cc)+".nii.gz");
-        //tmp.T->write("T_"+to_string(cc)+".nii.gz");
-        //tmp.p->write("p_"+to_string(cc)+".nii.gz");
-        //tmp.beta->write("beta_"+to_string(cc)+".nii.gz");
-    }
+		// fill, zero mean the timeseries
+		MatrixXd data(T, N);
+		ChunkConstIter<double> it(in); 
+		it.setLineChunk(3);
+		for(size_t xx=0; !it.eof(); it.nextChunk(), ++xx) {
+			double tmp = 0;
+			for(size_t tt=0; !it.eoc(); ++it) {
+				data(tt,xx) = *it;
+				tmp += *it;
+			}
+			tmp = 1./tmp;
+			for(size_t tt=0; !it.eoc(); ++it) 
+				data(tt,xx) = data(tt,xx)*tmp;
+		}
+
+		// perform PCA
+		std::cerr << "PCA...";
+		MatrixXd X_pc = pca(data, 0.01);
+		std::cerr << "Done " << endl;
+    
+		// perform ICA
+		std::cerr << "ICA...";
+		MatrixXd X_ic = ica(X_pc, 0.01);
+		std::cerr << "Done" << endl;
+
+	} else {
+		// SVD Method (since presumably there will be more timepoints than
+		// voxels)
+		cout << "Performing Temporal ICA" << endl;
+
+		MatrixXd data;
+		size_t times = 0;
+		size_t voxels = 0;
+		size_t row = 0;
+		size_t col = 0;
+		size_t rr = 0;
+		size_t cc = 0;
+		for(size_t ii = 0; ii<a_in.getValue().size(); ++ii) {
+			auto tmp = readMRImage(a_in.getValue()[ii]);
+			data.resize(data.rows()+times, data.cols()+voxels);
+
+			rr = 0;
+			cc = 0;
+			for(Vector3DIter<double> it(tmp); !it.eof(); ++it) {
+				cc = 0;
+				for(size_t tt=0; tt<tmp->tlen(); cc++, tt++) {
+					data(times+tt, 
+				}
+			}
+			
+			// fill starting at T, V
+			times += tmp->tlen();
+			voxels += tmp->elements()/tmp->tlen();
+			
+			col++;
+			if(col >= a_time_append.getValue())
+				col = 0;
+		}
+		
+		// fill Matrix with values from input
+		size_t T = in->tlen();
+		size_t N = in->elements()/T;
+
+		// fill, zero mean the timeseries
+		MatrixXd data(T, N);
+		ChunkConstIter<double> it(in); 
+		it.setLineChunk(3);
+		for(size_t xx=0; !it.eof(); it.nextChunk(), ++xx) {
+			double tmp = 0;
+			for(size_t tt=0; !it.eoc(); ++it) {
+				data(tt,xx) = *it;
+				tmp += *it;
+			}
+			tmp = 1./tmp;
+			for(size_t tt=0; !it.eoc(); ++it) 
+				data(tt,xx) = data(tt,xx)*tmp;
+		}
+
+		// perform PCA
+		std::cerr << "PCA...";
+		MatrixXd X_pc = pca(data, 0.01);
+		std::cerr << "Done " << endl;
+    
+		// perform ICA
+		std::cerr << "ICA...";
+		MatrixXd X_ic = ica(X_pc, 0.01);
+		std::cerr << "Done" << endl;
+	}
+
+//    // 
+//	MatrixXd regressors = reduce(inimg);
+//    for(size_t cc = 0; cc < regressors.cols(); cc++) {
+//        // perform regression
+//        //RegrResult tmp = regress(inimg, regressors.row(cc));
+//
+//        // write out each of the images
+//        //tmp.rsqr->write("rsqr_"+to_string(cc)+".nii.gz");
+//        //tmp.T->write("T_"+to_string(cc)+".nii.gz");
+//        //tmp.p->write("p_"+to_string(cc)+".nii.gz");
+//        //tmp.beta->write("beta_"+to_string(cc)+".nii.gz");
+//    }
 
 	} catch (TCLAP::ArgException &e)  // catch any exceptions
 	{ std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; }
