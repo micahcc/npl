28ce6b5063716ae6f4cb3fe40b2c37e5643fdd9f
Micah Chambers (mp2) <micahcc@ucla.edu>
2015-02-19 19:00:20 -0800

    bumped version

--- lib/version.h
+++ lib/version.h
@@ -1,9 +1,9 @@
 #ifndef __version__
 	#if defined(DEBUG)
-		#define __version__ "3.1.2-debug"
+		#define __version__ "3.1.3-debug"
 	#elif defined(NDEBUG)
-		#define __version__ "3.1.2-release"
+		#define __version__ "3.1.3-release"
 	#else
-		#define __version__ "3.1.2"
+		#define __version__ "3.1.3"
 	#endif
 #endif
