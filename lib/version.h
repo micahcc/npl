091bb38cabe25d00fe89ab576bb0ed1abaaea7bb
Micah Chambers (mp2) <micahcc@ucla.edu>
2015-02-20 17:00:36 -0800

    version bump for fixing probability

--- lib/version.h
+++ lib/version.h
@@ -1,9 +1,9 @@
 #ifndef __version__
 	#if defined(DEBUG)
-		#define __version__ "3.1.3-debug"
+		#define __version__ "3.1.4-debug"
 	#elif defined(NDEBUG)
-		#define __version__ "3.1.3-release"
+		#define __version__ "3.1.4-release"
 	#else
-		#define __version__ "3.1.3"
+		#define __version__ "3.1.4"
 	#endif
 #endif
