892ad37719d28eb292b58350d74fc6be371704af
Micah Chambers (scrapper) <micahcc@ucla.edu>
2015-03-17 10:16:24 -0700

    bumped version to reflect GLM stuff is done and all the basic fMRI processes have unit tests

--- lib/version.h
+++ lib/version.h
@@ -1,9 +1,9 @@
 #ifndef __version__
 	#if defined(DEBUG)
-		#define __version__ "3.1.4-debug"
+		#define __version__ "3.1.5-debug"
 	#elif defined(NDEBUG)
-		#define __version__ "3.1.4-release"
+		#define __version__ "3.1.5-release"
 	#else
-		#define __version__ "3.1.4"
+		#define __version__ "3.1.5"
 	#endif
 #endif
